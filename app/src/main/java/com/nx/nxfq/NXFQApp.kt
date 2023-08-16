94015cb793540c375d87301e7fbd90ac390166e1
nx <nxbitakinema@gmail.com>
2023-08-16 12:52:25 +0700

    add hilt, migrate file, build empty package, test run app >>> ok

---
+++ app/src/main/java/com/nx/nxfq/NXFQApp.kt
@@ -0,0 +1,7 @@
+package com.nx.nxfq
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NXFQApp: Application()
--- app/src/main/java/com/nx/nxfq/presentation/MainActivity.kt
+++ app/src/main/java/com/nx/nxfq/presentation/MainActivity.kt
@@ -1,16 +1,18 @@
-package com.nx.nxfq
+package com.nx.nxfq.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Text(text = "TEST RUN APP 01")
+            Text(text = "TEST RUN APP 02")
 
         }
     }
