6ac0bdf01bcaf0b2f2395f901f1b92955ca06468
nx <nxbitakinema@gmail.com>
2023-08-17 11:48:23 +0700

    final edit

--- app/src/main/java/com/nx/nxfq/presentation/qoute_screen/component/QouteText.kt
+++ app/src/main/java/com/nx/nxfq/presentation/qoute_screen/component/QouteText.kt
@@ -3,10 +3,12 @@ package com.nx.nxfq.presentation.qoute_screen.component
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun QouteText(
     qouteText: String
@@ -16,6 +18,17 @@ fun QouteText(
         color = Color.DarkGray,
         fontWeight = FontWeight.ExtraBold,
         fontSize = 28.sp,
-        fontStyle = FontStyle.Italic
+        fontStyle = FontStyle.Italic,
+
+
+        //  this is text - gradient color
+
+//        style = TextStyle(
+//            brush = Brush.linearGradient(
+//                colors = listOf(Color.Magenta, Color.Cyan) ))
+
+
     )
 }
+
+
